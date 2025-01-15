0b95f712fe3754c0294d6c329f47c6f09d520e17
AbhimanyuPratapSingh1803 <abhiheller1803@gmail.com>
2025-01-15 12:16:42 +0530

    Addedd responsiveness to Project section

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -17,14 +17,14 @@ const ProjectCard = ({ imgSrc, techs, currTechs, Title, description, gitLink, li
             data-aos="fade-up"
             data-aos-easing="ease-in-out-sine"
             data-aos-duration="500"
-            className="w-full md:w-1/2 z-9"
+            className="w-full min-[450px]:w-11/12 sm:w-3/4 md:w-2/3 lg:w-1/2 z-9"
         >
             <div
                 onMouseLeave={() => setHover(false)}
                 onMouseEnter={() => setHover(true)}
-                className={`p-8 font-poppins hover:scale-105 hover:shadow-xl hover:shadow-cyan-800 bg-[#000319] transition-all border-[1px] shadow-2xl shadow-cyan-950 border-slate-800 flex flex-col items-start justify-center gap-5 rounded-2xl`}
+                className={`p-5 sm:p-8 font-poppins hover:scale-105 hover:shadow-xl hover:shadow-cyan-800 bg-[#000319] transition-all border-[1px] shadow-2xl shadow-cyan-950 border-slate-800 flex flex-col items-start justify-center gap-5 rounded-2xl`}
             >
-                <div className="rounded-3xl h-72 px-12 pt-5 overflow-y-hidden overflow-x-hidden bg-gradient-to-r backdrop-blur-lg from-gray-900 via-cyan-950 to-slate-900 p-4">
+                <div className="rounded-3xl max-h-72 pt-5 overflow-y-hidden overflow-x-hidden bg-gradient-to-r backdrop-blur-lg from-gray-900 via-cyan-950 to-slate-900 p-3 sm:p-4">
                     <img
                         className={`rounded-xl ${hover ? "scale-105" : "scale-100"} rotate-3 transition-all shadow-xl shadow-cyan-950`}
                         src={imgSrc}
@@ -32,8 +32,8 @@ const ProjectCard = ({ imgSrc, techs, currTechs, Title, description, gitLink, li
                     />
                 </div>
                 <div className="flex flex-col p-3 gap-3 items-start justify-center">
-                    <p className="text-2xl text-cyan-300 font-semibold">{Title}</p>
-                    <p className="text-xl text-gray-500">{description}</p>
+                    <p className="text-xl min-[400px]:text-2xl min-[450px]:text-2xl sm:text-2xl text-cyan-300 font-semibold">{Title}</p>
+                    <p className="text-base min-[400px]:text-lg min-[450px]:text-xl text-gray-500">{description}</p>
                 </div>
                 <div className="flex items-center justify-between w-full">
                     <div
@@ -45,7 +45,7 @@ const ProjectCard = ({ imgSrc, techs, currTechs, Title, description, gitLink, li
                                 key={idx}
                                 className="relative avatar cursor-pointer bg-slate-800 border-[2px] border-slate-600 rounded-full p-[5px]"
                             >
-                                <div className="w-7">
+                                <div className="w-[22px] min-[450px]:w-7">
                                     <img className="rounded-btn" src={techs[val].img} />
                                 </div>
                             </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,23 +5,39 @@ import RevealText from "../components/RevealText";
 const Projects = ({techs}) => {
 
     return (
-        <div id="projects" className="min-h-screen w-screen px-10 sm:px-20 lg:px-32 flex flex-col items-center justify-start gap-20 pb-20 pt-20">
+        <div id="projects" className="min-h-screen w-screen px-5 sm:px-8 lg:px-14 xl:px-32 flex flex-col items-center justify-start gap-20 pb-20 pt-20">
             <div class="absolute top-0 left-0 w-full h-16 bg-gradient-to-b from-[#000319] to-transparent pointer-events-none"></div>
-            <div className="flex flex-col items-center justify-center gap-4">
+            <div className="flex flex-col items-center justify-center sm:gap-4 min-[500px]:px-5">
                 <RevealText
                     text="Here are some of my Projects"
                     duration={0.5}
-                    extraClass="font-semibold text-5xl text-cyan-300"
+                    extraClass="font-semibold hidden sm:flex text-3xl min-[600px]:text-4xl md:text-5xl text-cyan-300"
+                />
+                <RevealText
+                    text="Here are some of"
+                    duration={0.5}
+                    extraClass="font-semibold flex sm:hidden text-4xl min-[500px]:text-5xl text-cyan-300"
+                />
+                <RevealText
+                    text="My Projects"
+                    duration={0.5}
+                    extraClass="font-semibold flex sm:hidden text-4xl min-[500px]:text-5xl text-cyan-300"
                 />
                 <RevealText
                     text="Featured projects developed with passion to drive results."
                     duration={0.2}
                     delayTime={0.01}
-                    extraClass="font-semibold text-lg text-slate-500"
+                    extraClass="font-semibold hidden sm:flex text-sm min-[600px]:text-base md:text-lg text-slate-500"
+                />
+                <RevealText
+                    text="Featured projects developed with passion."
+                    duration={0.2}
+                    delayTime={0.01}
+                    extraClass="font-semibold flex sm:hidden mt-5 text-xs min-[500px]:text-sm text-slate-500"
                 />
             </div>
             <div className="flex flex-col items-center justify-center gap-20 w-full">
-                <div className="flex md:flex-row flex-col items-center w-full justify-center gap-10">
+                <div className="flex xl:flex-row flex-col items-center w-full justify-center gap-10">
                     <ProjectCard
                         techs={techs}
                         currTechs={[3,4,2,5,7,8]}
@@ -38,10 +54,10 @@ const Projects = ({techs}) => {
                         Title="MindScribe - Notes App"
                         gitLink = "https://github.com/AbhimanyuPratapSingh1803/MindScribe"
                         liveLink = "https://mindscribe-alpha.vercel.app/"
-                        description="A Notes Application to capture your thoughts, organize your ideas, and turn inspiration into action."
+                        description="A Notes App to capture your thoughts, organize your ideas, and turn inspiration into action."
                     />
                 </div>
-                <div className="flex md:flex-row flex-col items-center w-full justify-center gap-10">
+                <div className="flex xl:flex-row flex-col items-center w-full justify-center gap-10">
                     <ProjectCard
                         techs={techs}
                         currTechs={[3,4,2,11]}
@@ -58,7 +74,7 @@ const Projects = ({techs}) => {
                         Title="Admin Dashboard"
                         gitLink = "https://github.com/AbhimanyuPratapSingh1803/Instinctive-assignment"
                         liveLink = "https://instinctive-assignment-red.vercel.app/"
-                        description="Admin Dashboard for empowering your decisions with insights, control, efficiency at your fingertips."
+                        description="Admin Dashboard for empowering your decisions with insights and efficiency at your fingertips."
                     />
                 </div>
             </div>
