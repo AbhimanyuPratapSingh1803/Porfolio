f0ff69349680d55f4492f86a71a458d2f6cb39d6
AbhimanyuPratapSingh1803 <abhiheller1803@gmail.com>
2025-01-15 13:10:32 +0530

    Added background on landing page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,12 +12,12 @@ const Navbar = () => {
             viewport={{ once: true }}
             transition={{ duration: 1, ease: "easeInOut", }}
             className='flex fixed md:absolute z-10 border-b-[1px] border-slate-800 backdrop-blur-xl items-center justify-between w-screen sm:px-10 px-5 py-5'>
-            <Logo extraClass="text-base sm:text-lg" text={"Developed by Abhimanyu Pratap Singh"}/>
+            <Logo extraClass="text-base text-white sm:text-lg" text={"Developed by Abhimanyu Pratap Singh"}/>
             <div className='font-poppins hidden md:flex gap-5 items-center justify-between h-fit w-fit'>
-                <a href="#about"><Para text={"About"} extraClass="font-medium text-lg min-[900px]:text-xl"/></a>
-                <a href="#projects"><Para text={"Projects"} extraClass="font-medium text-lg min-[900px]:text-xl"/></a>
-                <a href="#techStack"><Para text={"Tech-Stack"} extraClass="font-medium text-lg min-[900px]:text-xl"/></a>
-                <a href="#contact"><Para text={"Contact"} extraClass="font-medium text-lg min-[900px]:text-xl"/></a>
+                <a href="#about"><Para text={"About"} extraClass="font-medium text-white text-lg min-[900px]:text-xl"/></a>
+                <a href="#projects"><Para text={"Projects"} extraClass="font-medium text-white text-lg min-[900px]:text-xl"/></a>
+                <a href="#techStack"><Para text={"Tech-Stack"} extraClass="font-medium text-white text-lg min-[900px]:text-xl"/></a>
+                <a href="#contact"><Para text={"Contact"} extraClass="font-medium text-white text-lg min-[900px]:text-xl"/></a>
             </div>
         </motion.nav>
     )
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
+import flattenColorPalette from "tailwindcss/lib/util/flattenColorPalette";
 /** @type {import('tailwindcss').Config} */
-import daisyui from "daisyui"
+import daisyui from "daisyui";
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
@@ -7,7 +8,31 @@ export default {
             fontFamily: {
                 poppins: ["Poppins", "sans-serif"],
             },
+            animation: {
+                aurora: "aurora 60s linear infinite",
+            },
+            keyframes: {
+                aurora: {
+                    from: {
+                        backgroundPosition: "50% 50%, 50% 50%",
+                    },
+                    to: {
+                        backgroundPosition: "350% 50%, 350% 50%",
+                    },
+                },
+            },
         },
     },
-    plugins: [daisyui,],
+    plugins: [daisyui, addVariablesForColors],
 };
+
+function addVariablesForColors({ addBase, theme }) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+        Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+
+    addBase({
+        ":root": newVars,
+    });
+}
