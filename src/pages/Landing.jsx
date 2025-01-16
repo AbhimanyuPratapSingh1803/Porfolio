5d301da1b4765eb9c2993ccaff36975483daab25
AbhimanyuPratapSingh1803 <abhiheller1803@gmail.com>
2025-01-16 12:00:50 +0530

    addedd aurora background on landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import RevealText from "../components/RevealText";
+import {easeInOut, motion} from "framer-motion";
 import CustomButton from "../components/CustomButton";
-import Logo from "../components/Logo";
 import Navbar from "../components/Navbar";
-import { LampContainer } from "../components/ui/LampContainer";
+import { AuroraBackground } from "../components/ui/AuroraBackground";
 
 const Landing = () => {
     return (
-        <div className="h-[100vh] relative w-full">
+        <AuroraBackground>
+        <div className="h-[110vh] relative w-full">
             <Navbar />
-            <div className="relative h-full w-full">
-                <div className="relative flex h-full w-full items-center justify-center flex-col">
-                    <div className="absolute w-full top-16">
-                            <LampContainer>
-                            <div className="flex flex-col items-center justify-center">
-                                <RevealText
+            <div className=" h-full w-full">
+                <div className="flex h-full w-full items-center justify-center flex-col">
+                    <div className="w-full">
+                            <motion.div 
+                                initial={{ opacity: 0, y: 100 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                transition={{ duration: 1, ease: easeInOut }}
+                                className="flex flex-col items-center justify-center">
+                                {/* <RevealText
                                     delayTime={0.035}
                                     duration={0.5}
                                     extraClass="-mb-2 hidden md:flex text-[35px] md:text-[38px] min-[980px]:text-[50px] min-[1160px]:text-[60px] sm:mx-10 pb-0 text-white"
@@ -45,18 +49,21 @@ const Landing = () => {
                                     text={
                                         "Hi i'am Abhimanyu Pratap Singh, A Full Stack Developer"
                                     }
-                                />
+                                /> */}
+                                <p className="text-white font-bold text-2xl min-[400px]:text-[33px] min-[550px]:text-4xl sm:text-5xl text-center md:text-[38px] md:leading-[45px] lg:text-5xl xl:text-6xl font-poppins mx-5 min-[550px]:mx-10">Turning Imagination and Ideas into</p>
+                                <p className="text-cyan-300 font-bold text-3xl min-[400px]:text-4xl min-[550px]:text-5xl sm:text-6xl text-center md:text-[38px] md:leading-[45px] lg:text-5xl xl:text-6xl font-poppins mx-5 min-[550px]:mx-10 mb-5">Real Life Products.</p>
+                                <p className="text-slate-500 mx-5 min-[550px]:mx-10 font-poppins text-center text-xs min-[400px]:text-sm min-[550px]:text-base sm:text-lg xl:text-xl font-semibold">Hi i'am Abhimanyu Pratap Singh, A Full Stack Developer</p>
                                 <CustomButton
                                     text="Let's Connect"
                                     extraClass="mx-auto mt-5 min-[640px]:mt-10 max-[400px]:text-sm w-[150px] min-[400px]:w-[190px] md:w-[200px] py-2 sm:py-3 px-2 sm:px-10 text-black bg-cyan-300"
                                 />
-                            </div>
-                            </LampContainer>
+                            </motion.div>
                     </div>
                 </div>
             </div>
             <div class="absolute bottom-0 left-0 w-full h-16 bg-gradient-to-t from-[#000319] to-transparent pointer-events-none"></div>
         </div>
+        </AuroraBackground>
     );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,7 @@
-/** @type {import('tailwindcss').Config} */
+const {
+    default: flattenColorPalette,
+  } = require("tailwindcss/lib/util/flattenColorPalette");
+  /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
@@ -8,21 +11,29 @@ export default {
                 poppins: ["Poppins", "sans-serif"],
             },
             animation: {
-                spotlight: "spotlight 2s ease .75s 1 forwards",
+                aurora: "aurora 60s linear infinite",
             },
             keyframes: {
-                spotlight: {
-                    "0%": {
-                        opacity: 0,
-                        transform: "translate(-72%, -62%) scale(0.5)",
+                aurora: {
+                    from: {
+                        backgroundPosition: "50% 50%, 50% 50%",
                     },
-                    "100%": {
-                        opacity: 1,
-                        transform: "translate(-50%,-40%) scale(1)",
+                    to: {
+                        backgroundPosition: "350% 50%, 350% 50%",
                     },
                 },
             },
         },
     },
-    plugins: [daisyui],
+    plugins: [daisyui, addVariablesForColors],
 };
+function addVariablesForColors({ addBase, theme }) {
+    let allColors = flattenColorPalette(theme("colors"));
+    let newVars = Object.fromEntries(
+      Object.entries(allColors).map(([key, val]) => [`--${key}`, val])
+    );
+  
+    addBase({
+      ":root": newVars,
+    });
+  }
