db10390fe703e0e6fd6b6d4071256eb22b728993
AbhimanyuPratapSingh1803 <abhiheller1803@gmail.com>
2025-01-18 18:52:23 +0530

    addedd blob animation

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,64 +1,48 @@
 import React from "react";
 import RevealText from "../components/RevealText";
-import {easeInOut, motion} from "framer-motion";
+import { easeInOut, motion } from "framer-motion";
 import CustomButton from "../components/CustomButton";
 import { AuroraBackground } from "../components/ui/AuroraBackground";
 import { Link } from "react-router-dom";
 
-const Landing = ({links}) => {
+const Landing = ({ links }) => {
     return (
         // <AuroraBackground>
         <div id="home" className="h-[110vh] relative w-full">
             <div className=" h-full w-full">
-                <div className="flex h-full w-full items-center justify-center flex-col">
-                    <div className="w-full">
-                            <motion.div 
-                                initial={{ opacity: 0, y: 100 }}
-                                animate={{ opacity: 1, y: 0 }}
-                                transition={{ duration: 1, ease: easeInOut }}
-                                className="flex flex-col items-center mb-14 justify-center">
-                                {/* <RevealText
-                                    delayTime={0.035}
-                                    duration={0.5}
-                                    extraClass="-mb-2 hidden md:flex text-[35px] md:text-[38px] min-[980px]:text-[50px] min-[1160px]:text-[60px] sm:mx-10 pb-0 text-white"
-                                    text={"Turning Imagination and Ideas into"}
+                <div className="flex relative h-full w-full items-center justify-center flex-col">
+                    <motion.div
+                        initial={{ opacity: 0,}}
+                        animate={{ opacity: 1,}}
+                        transition={{ duration: 1.5, ease: easeInOut }}
+                    >
+                        <div className="absolute z-0 animate-blob animation-delay-2000 bg-yellow-500 mix-blend-screen filter blur-2xl opacity-50 size-36 min-[400px]:size-44 min-[450px]:size-48 min-[500px]:size-56 sm:size-64 md:size-72 rounded-full top-1/4 left-[7%] min-[450px]:left-[15%] md:left-[25%]"></div>
+                        <div className="absolute z-0 animate-blob animation-delay-4000 bg-orange-400 mix-blend-screen filter blur-2xl opacity-50 size-36 min-[400px]:size-44 min-[450px]:size-48 min-[500px]:size-56 sm:size-64 md:size-72 rounded-full top-[25%] left-[35%] md:left-[40%]"></div>
+                        <div className="absolute z-0 animate-blob bg-blue-500 mix-blend-screen filter blur-2xl opacity-50 size-36 min-[400px]:size-44 min-[450px]:size-48 min-[500px]:size-56 sm:size-64 md:size-72 rounded-full top-1/3 left-[45%] min-[450px]:left-[45%] min-[500px]:left-[50%] md:left-[55%]"></div>
+                    </motion.div>
+                    <div className="w-full absolute z-1">
+                        <motion.div
+                            initial={{ opacity: 0, y: 100 }}
+                            animate={{ opacity: 1, y: 0 }}
+                            transition={{ duration: 1, ease: easeInOut }}
+                            className="flex flex-col items-center mb-14 justify-center">
+                            <p className="text-white font-bold text-2xl min-[400px]:text-[33px] min-[550px]:text-4xl sm:text-5xl text-center md:text-[38px] md:leading-[45px] lg:text-5xl xl:text-6xl font-poppins mx-5 min-[550px]:mx-10">
+                                Turning Imagination and Ideas into
+                            </p>
+                            <p className="text-cyan-300 font-bold text-3xl min-[400px]:text-4xl min-[550px]:text-5xl sm:text-6xl text-center md:text-[38px] md:leading-[45px] lg:text-5xl xl:text-6xl font-poppins mx-5 min-[550px]:mx-10 mb-5">
+                                Real Life Products.
+                            </p>
+                            <p className="text-slate-300 mx-5 min-[550px]:mx-10 font-poppins text-center text-xs min-[400px]:text-sm min-[550px]:text-base sm:text-lg xl:text-xl font-semibold">
+                                Hi i'am Abhimanyu Pratap Singh, A Full Stack
+                                Developer
+                            </p>
+                            <Link to={"mailto:abhimanyusingh1533@gmail.com"}>
+                                <CustomButton
+                                    text="Let's Connect"
+                                    extraClass="mx-auto mt-5 min-[640px]:mt-10 max-[400px]:text-sm w-[150px] min-[400px]:w-[190px] md:w-[200px] py-2 sm:py-3 px-2 sm:px-10 text-black bg-cyan-300"
                                 />
-                                <RevealText
-                                    delayTime={0.035}
-                                    duration={0.5}
-                                    extraClass="-mb-2 w-full text-[30px] min-[400px]:text-[33px] min-[520px]:text-[40px] md:hidden pb-0 text-white"
-                                    text={"Turning Imagination"}
-                                />
-                                <RevealText
-                                    delayTime={0.035}
-                                    duration={0.5}
-                                    extraClass="-mb-1 w-full text-[30px] min-[400px]:text-[35px] min-[520px]:text-[40px] md:hidden pb-0 text-white"
-                                    text={"and Ideas into"}
-                                />
-                                <RevealText
-                                    delayTime={0.035}
-                                    duration={0.5}
-                                    extraClass="text-[32px] min-[400px]:text-[38px] min-[520px]:text-[50px] md:text-[50px] min-[1160px]:text-[60px] text-cyan-300"
-                                    text={"Real Life Products."}
-                                />
-                                <RevealText
-                                    delayTime={0.009}
-                                    duration={0.5}
-                                    extraClass="text-sm min-[400px]:text-base mx-5 md:text-xl text-slate-500 pt-5 text-center"
-                                    text={
-                                        "Hi i'am Abhimanyu Pratap Singh, A Full Stack Developer"
-                                    }
-                                /> */}
-                                <p className="text-white font-bold text-2xl min-[400px]:text-[33px] min-[550px]:text-4xl sm:text-5xl text-center md:text-[38px] md:leading-[45px] lg:text-5xl xl:text-6xl font-poppins mx-5 min-[550px]:mx-10">Turning Imagination and Ideas into</p>
-                                <p className="text-cyan-300 font-bold text-3xl min-[400px]:text-4xl min-[550px]:text-5xl sm:text-6xl text-center md:text-[38px] md:leading-[45px] lg:text-5xl xl:text-6xl font-poppins mx-5 min-[550px]:mx-10 mb-5">Real Life Products.</p>
-                                <p className="text-slate-500 mx-5 min-[550px]:mx-10 font-poppins text-center text-xs min-[400px]:text-sm min-[550px]:text-base sm:text-lg xl:text-xl font-semibold">Hi i'am Abhimanyu Pratap Singh, A Full Stack Developer</p>
-                                <Link to={"mailto:abhimanyusingh1533@gmail.com"}>
-                                    <CustomButton
-                                        text="Let's Connect"
-                                        extraClass="mx-auto mt-5 min-[640px]:mt-10 max-[400px]:text-sm w-[150px] min-[400px]:w-[190px] md:w-[200px] py-2 sm:py-3 px-2 sm:px-10 text-black bg-cyan-300"
-                                    />
-                                </Link>
-                            </motion.div>
+                            </Link>
+                        </motion.div>
                     </div>
                 </div>
             </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ const {
   } = require("tailwindcss/lib/util/flattenColorPalette");
   /** @type {import('tailwindcss').Config} */
 import daisyui from "daisyui";
+import { transform } from "lodash";
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
@@ -12,8 +13,23 @@ export default {
             },
             animation: {
                 aurora: "aurora 60s linear infinite",
+                blob : "blob 7s linear infinite"
             },
             keyframes: {
+                blob : {
+                    "0%" : {
+                        transform : "translate(0px, 0px) scale(1)",
+                    },
+                    "33%" : {
+                        transform : "translate(30px, -50px) scale(1)",
+                    },
+                    "66%" : {
+                        transform : "translate(-20px, 20px) scale(1)",
+                    },
+                    "100%" : {
+                        transform : "translate(0px, 0px) scale(1)",
+                    }
+                },
                 aurora: {
                     from: {
                         backgroundPosition: "50% 50%, 50% 50%",
